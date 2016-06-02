19cc28ce5e09fcacf8c56844877ce608eda996f9
micah <mayoms@gmail.com>
2016-06-01 20:21:35 -0700

    refactored hash based algorithm, added comments

--- eulerian_path/eulerian.py
+++ eulerian_path/eulerian.py
@@ -1,9 +1,24 @@
+# This is my first solution for finding a eulerian tour. The input is a list of edges
+# represented as paired tuples e.g. (1,2). It is assumed that the list contains
+# a complete tour, so no testing is done to verify criteria.
+
+
+# A helper fuction that finds the next node, takes 2 parameters, an edge (pair of nodes)
+# to evaluate, and the current node position. If the current position is part of the node
+# it returns the next position in the graph, otherwise returns the current node.
+
 def next_node(edge, pos):
   if pos in edge:
     return [x for x in edge if x != pos][0]
   else: 
     return pos
     
+
+# A valid path consists of at least one circuit that terminates at the starting
+# node. This function takes two parameters, a graph in the form of a list of edges
+# and the starting node. It traverses the graph until the starting node is encountered
+# and then returns the completed 'leg', as well as the remaining unvisited edges.
+
 def find_leg(graph, start):
     
   path = [start]
@@ -22,6 +37,11 @@ def find_leg(graph, start):
       graph.append(edge)
   return path, graph
         
+# this is the organizing function - it takes the graph as its input and 
+# then using the find_leg function, finds valid circuits within the graph.
+# If there is more than one circuit in the path, the subsequent nodes 
+# are circular, so we grab the first point where the circuits can be joined
+# and shift the leg so it can be inserted at the correct spot in the existing path.
 
 def find_eulerian_tour(graph):
 
@@ -36,17 +56,12 @@ def find_eulerian_tour(graph):
     path = path[:pos+1] + leg[1:] + path[pos+1:]
          
   return path
+
+# Test Cases
             
 graph = [(1, 2), (2, 3), (3, 1)]
 graph2 = [(0, 1), (1, 5), (1, 7), (4, 5), (4, 8), (1, 6), (3, 7), (5, 9), (2, 4), (0, 4), (2, 5), (3, 6), (8, 9)]
 graph3 = [(1, 13), (1, 6), (6, 11), (3, 13), (8, 13), (0, 6), (8, 9),(5, 9), (2, 6), (6, 10), (7, 9), (1, 12), (4, 12), (5, 14), (0, 1),  (2, 3), (4, 11), (6, 9), (7, 14),  (10, 13)]
-
-
-
-
-
-
-
 
 print find_eulerian_tour(graph)
 print find_eulerian_tour(graph2)
--- eulerian_path/eulerian2.py
+++ eulerian_path/eulerian2.py
@@ -1,4 +1,11 @@
+# This is the second implementation of the eulerian tour algorithm. In this 
+# version.
+
 from collections import defaultdict
+
+
+# We first reorganize the list of nodes into a hash of nodes, which point
+# to a list of adjoining nodes. This is accomplished witht he make_graph_hash function.
 
 def make_graph_hash(graph):
 
@@ -10,31 +17,29 @@ def make_graph_hash(graph):
 
   return edge_dict
 
+# this function starts with the first node in the graph hash and follows the
+# path until there are no steps left. Since visiting an edge can be bidirectional
+# we need to remove the reference to the edge in the adjacent edge list.
+
 def get_eulerian_path(graph):
 
   graph_hash = make_graph_hash(graph)
-
-  keys = list(graph_hash)
-  path = []
-  
-  for key in keys:                  
-    if graph_hash[key]:
-      path.append(key)
-      current = graph_hash[key].pop()
-    if not graph_hash[current]:
-        break
-    while True:
-      path.append(current)
-      if key in graph_hash[current]:
-          graph_hash[current].remove(key)
-      if graph_hash[current]:
-        next = graph_hash[current].pop()
-        key = current
-        current = next
+  path = [] 
+  for node in graph_hash:
+    if graph_hash[node]:
+      path.append(node)
+      next_node = graph_hash[node].pop()
+      while graph_hash[next_node]:
+        path.append(next_node)
+        graph_hash[next_node].remove(node)
+        if graph_hash[next_node]:
+          node = next_node
+          next_node = graph_hash[next_node].pop()
       else:
         break
-
   return path
+
+# test cases
 
 graph = [(1, 2), (2, 3), (3, 1)]
 graph2 = [(0, 1), (1, 5), (1, 7), (4, 5), (4, 8), (1, 6), (3, 7), (5, 9), (2, 4), (0, 4), (2, 5), (3, 6), (8, 9)]
