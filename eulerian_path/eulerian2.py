6c4cd9a1252bdddb7d9dfbeddf7d2365eaf97954
micah <mayoms@gmail.com>
2016-06-01 21:09:02 -0700

    updated dict/hash algorithm to remove unneeded steps

--- eulerian_path/eulerian2.py
+++ eulerian_path/eulerian2.py
@@ -1,5 +1,6 @@
 # This is the second implementation of the eulerian tour algorithm. In this 
-# version.
+# version we use a hash to represent the graph, rather than a list of joining
+# nodes. 
 
 from collections import defaultdict
 
@@ -21,22 +22,20 @@ def make_graph_hash(graph):
 # path until there are no steps left. Since visiting an edge can be bidirectional
 # we need to remove the reference to the edge in the adjacent edge list.
 
-def get_eulerian_path(graph):
+def get_eulerian_path(graph_hash):
+  
+  path = []
+  
+  node = graph_hash.keys()[0]
+  path.append(node)
+  next_node = graph_hash[node].pop()
 
-  graph_hash = make_graph_hash(graph)
-  path = [] 
-  for node in graph_hash:
-    if graph_hash[node]:
-      path.append(node)
-      next_node = graph_hash[node].pop()
-      while graph_hash[next_node]:
-        path.append(next_node)
-        graph_hash[next_node].remove(node)
-        if graph_hash[next_node]:
-          node = next_node
-          next_node = graph_hash[next_node].pop()
-      else:
-        break
+  while graph_hash[next_node]:
+    path.append(next_node)
+    graph_hash[next_node].remove(node)
+    if graph_hash[next_node]:
+      node = next_node
+      next_node = graph_hash[next_node].pop()
   return path
 
 # test cases
@@ -44,6 +43,10 @@ def get_eulerian_path(graph):
 graph = [(1, 2), (2, 3), (3, 1)]
 graph2 = [(0, 1), (1, 5), (1, 7), (4, 5), (4, 8), (1, 6), (3, 7), (5, 9), (2, 4), (0, 4), (2, 5), (3, 6), (8, 9)]
 graph3 = [(1, 13), (1, 6), (6, 11), (3, 13), (8, 13), (0, 6), (8, 9),(5, 9), (2, 6), (6, 10), (7, 9), (1, 12), (4, 12), (5, 14), (0, 1),  (2, 3), (4, 11), (6, 9), (7, 14),  (10, 13)]
+
+graph = make_graph_hash(graph)
+graph2 = make_graph_hash(graph2)
+graph3 = make_graph_hash(graph3)
 
 print get_eulerian_path(graph)
 print get_eulerian_path(graph2)
